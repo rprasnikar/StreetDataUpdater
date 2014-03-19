09c778569dc43bd577c36008bad9be11138ec100
prasnr <prasnr@AT-NB-PRASNR.at.softlab.net>
2014-03-19 18:09:48 +0100

    File abholen in Inhalt chekcen

--- src/main/java/com/nttdata/occ/streetdataupdater/App.java
+++ src/main/java/com/nttdata/occ/streetdataupdater/App.java
@@ -1,13 +1,35 @@
 package com.nttdata.occ.streetdataupdater;
 
-/**
- * Hello world!
- *
- */
+import java.io.*;
+import java.net.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipEntry;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String s = "http://www.statistik.at/verzeichnis/strassenliste/gemplzstr.zip";
+        URL u;
+        InputStream is = null;
+        ZipInputStream zis;
+        try
+        {
+                u = new URL(s);
+                is = u.openStream();
+                zis = new ZipInputStream(is);
+                ZipEntry entry;
+                while ((entry = zis.getNextEntry()) != null)
+                {
+                    System.out.println("entry: " + entry.getName() + ", " + entry.getSize());
+
+        }
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }        
     }
 }
