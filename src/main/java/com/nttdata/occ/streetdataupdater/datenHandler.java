0652d29cdb4c87ad3341d6762cbd95c500567e22
robert <robert@192.168.88.20>
2014-04-21 22:36:18 +0200

    geht doch mit SAX

--- src/main/java/com/nttdata/occ/streetdataupdater/datenHandler.java
+++ src/main/java/com/nttdata/occ/streetdataupdater/datenHandler.java
@@ -16,6 +16,10 @@ import org.xml.sax.SAXParseException;
  * @author robert
  */
 public class datenHandler extends DefaultHandler {
+    private Integer isDatensatz = 0;
+    private Integer isElement = 0;
+    private Integer elementIndex = 0;
+    
   @Override
   public void startDocument()
   {
@@ -32,15 +36,33 @@ public class datenHandler extends DefaultHandler {
   public void startElement( String namespaceURI, String localName,
                             String qName, Attributes atts )
   {
-    System.out.println( "namespaceURI: " + namespaceURI );
-    System.out.println( "localName: " + localName );
+//    System.out.println( "namespaceURI: " + namespaceURI );
+//    System.out.println( "localName: " + localName );
     System.out.println( "qName: " + qName );
     for ( int i = 0; i < atts.getLength(); i++ )
       System.out.printf( "Attribut no. %d: %s = %s%n", i,
                          atts.getQName( i ), atts.getValue( i ) );
     if ("datensatz".equals(qName)) {
         System.out.println("Neuer Datensatz...");
-        System.out.println(atts.getLength());
+//        System.out.println(atts.getLength());
+        elementIndex = 1;
+    }
+    
+        if ("element".equals(qName)) {
+        System.out.println("Element " + elementIndex++);
+    }
+  }
+  
+    public void endElement( String namespaceURI, String localName,
+                            String qName )
+  {
+//    System.out.println( "namespaceURI: " + namespaceURI );
+//    System.out.println( "localName: " + localName );
+//    System.out.println( "qName: " + qName );
+    
+    if ("datensatz".equals(qName)) {
+        System.out.println("Ende neuer Datensatz...");
+        elementIndex = 0;
     }
   }
   
@@ -51,14 +73,15 @@ public class datenHandler extends DefaultHandler {
 // To get idea which element the reader is in a home made stack would be needed 
 // or simple impl. with isElementXY = true in startElement and isElementXY = false in endElement     
 
-//      System.out.println( "String: " + new String(ch));
-//
+      if (elementIndex > 1)
+          System.out.println( "String: " + new String(ch, start, length).trim());
+
 //      System.out.println( "Characters:" );
 //
 //    for ( int i = start; i < (start + length); i++ )
 //      System.out.printf( "%1$c (%1$x) ", (int) ch[i] );
 
-    System.out.println("String...");
+//    System.out.println("String...");
   }
 public void error( SAXParseException e ) throws SAXException
 {
